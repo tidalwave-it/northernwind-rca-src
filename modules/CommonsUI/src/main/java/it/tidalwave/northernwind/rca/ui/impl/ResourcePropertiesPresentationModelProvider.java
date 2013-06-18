0a3d6dc0ec4d08bc27a5babe148c67f72b48c3d8
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-18 08:13:35 +0200

    ...

--- modules/CommonsUI/src/main/java/it/tidalwave/northernwind/rca/ui/impl/ResourcePropertiesPresentationModelProvider.java
+++ modules/CommonsUI/src/main/java/it/tidalwave/northernwind/rca/ui/impl/ResourcePropertiesPresentationModelProvider.java
@@ -1,13 +1,32 @@
 /*
  * #%L
+ * *********************************************************************************************************************
+ *
+ * NorthernWind - lightweight CMS
+ * http://northernwind.tidalwave.it - hg clone https://bitbucket.org/tidalwave/northernwind-src
  * %%
+ * Copyright (C) 2011 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
  * #L%
  */
-
 package it.tidalwave.northernwind.rca.ui.impl;
 
-import it.tidalwave.role.ui.PresentationModelProvider;
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,6 +37,7 @@ import it.tidalwave.util.Key;
 import it.tidalwave.util.spi.SimpleFinderSupport;
 import it.tidalwave.role.spi.DefaultSimpleComposite;
 import it.tidalwave.role.ui.PresentationModel;
+import it.tidalwave.role.ui.PresentationModelProvider;
 import it.tidalwave.role.ui.RowHashMap;
 import it.tidalwave.dci.annotation.DciRole;
 import it.tidalwave.northernwind.core.model.ResourceProperties;
