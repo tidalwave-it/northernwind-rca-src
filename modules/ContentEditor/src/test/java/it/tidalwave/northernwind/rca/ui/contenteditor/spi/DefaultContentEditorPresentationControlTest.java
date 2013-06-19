94a779e86c2008e30913365d67d33aadc4fa2e2e
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-19 18:45:17 +0200

    Added test.

--- modules/ContentEditor/src/test/java/it/tidalwave/northernwind/rca/ui/contenteditor/spi/DefaultContentEditorPresentationControlTest.java
+++ modules/ContentEditor/src/test/java/it/tidalwave/northernwind/rca/ui/contenteditor/spi/DefaultContentEditorPresentationControlTest.java
@@ -27,11 +27,17 @@
  */
 package it.tidalwave.northernwind.rca.ui.contenteditor.spi;
 
-import it.tidalwave.messagebus.annotation.SimpleMessageSubscriber;
-import it.tidalwave.northernwind.rca.ui.contenteditor.ContentEditorPresentation;
-import it.tidalwave.northernwind.rca.ui.event.ContentSelectedEvent;
+import java.io.IOException;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
+import it.tidalwave.role.ui.PresentationModel;
+import it.tidalwave.role.ui.PresentationModelProvider;
+import it.tidalwave.messagebus.annotation.SimpleMessageSubscriber;
+import it.tidalwave.northernwind.core.model.ResourceProperties;
+import it.tidalwave.northernwind.model.impl.admin.AdminContent;
+import it.tidalwave.northernwind.rca.ui.contenteditor.ContentEditorPresentation;
+import it.tidalwave.northernwind.rca.ui.event.ContentSelectedEvent;
+import static it.tidalwave.northernwind.rca.ui.contenteditor.spi.DefaultContentEditorPresentationControl.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.*;
@@ -48,6 +54,14 @@ public class DefaultContentEditorPresentationControlTest
 
     private ContentEditorPresentation presentation;
 
+    private AdminContent content; // FIXME: use Content
+
+    private ResourceProperties properties;
+
+    private PresentationModelProvider pmProvider;
+
+    private PresentationModel pm;
+
     /*******************************************************************************************************************
      *
      ******************************************************************************************************************/
@@ -56,6 +70,15 @@ public class DefaultContentEditorPresentationControlTest
       {
         fixture = new DefaultContentEditorPresentationControl();
         presentation = mock(ContentEditorPresentation.class);
+        content = mock(AdminContent.class);
+        properties = mock(ResourceProperties.class);
+        pmProvider = mock(PresentationModelProvider.class);
+        pm = mock(PresentationModel.class);
+
+        when(content.getProperties()).thenReturn(properties);
+        when(pmProvider.createPresentationModel(anyVararg())).thenReturn(pm);
+        when(properties.as(eq(PresentationModelProvider.class))).thenReturn(pmProvider);
+
         fixture.initialize(presentation);
       }
 
@@ -84,10 +107,31 @@ public class DefaultContentEditorPresentationControlTest
     public void must_clear_the_presentation_on_reception_of_an_empty_selection()
       {
         reset(presentation);
-        
+
         fixture.onContentSelected(new ContentSelectedEvent());
 
         verify(presentation).clear();
         verifyNoMoreInteractions(presentation);
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void must_populate_the_presentation_on_reception_of_selected_content()
+      throws IOException
+      {
+        when(properties.getProperty(eq(PROPERTY_FULL_TEXT), anyString())).thenReturn("full text");
+        when(properties.getProperty(eq(PROPERTY_TITLE), anyString())).thenReturn("title");
+
+        reset(presentation);
+
+        fixture.onContentSelected(new ContentSelectedEvent(content));
+
+        verify(presentation).populateProperties(same(pm));
+        verify(presentation).showUp();
+        verifyNoMoreInteractions(presentation);
+        assertThat(fixture.fields.document.get(), is("full text"));
+        assertThat(fixture.fields.title.get(), is("title"));
       }
   }
