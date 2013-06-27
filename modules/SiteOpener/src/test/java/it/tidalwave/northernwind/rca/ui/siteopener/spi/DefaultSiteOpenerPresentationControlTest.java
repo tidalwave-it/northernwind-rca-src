90f0a8dcf529b0632a4a6c46762d20644ceb01c1
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-06-27 22:25:33 +0200

    ...

--- modules/SiteOpener/src/test/java/it/tidalwave/northernwind/rca/ui/siteopener/spi/DefaultSiteOpenerPresentationControlTest.java
+++ modules/SiteOpener/src/test/java/it/tidalwave/northernwind/rca/ui/siteopener/spi/DefaultSiteOpenerPresentationControlTest.java
@@ -86,7 +86,7 @@ public class DefaultSiteOpenerPresentationControlTest
     /*******************************************************************************************************************
      *
      ******************************************************************************************************************/
-    @Test
+    @Test(dependsOnMethods = "initialize_must_bind_the_presentation_and_set_the_default_path_to_user_home")
     public void must_fire_an_OpenSiteEvent_when_action_performed_and_the_user_selected_a_folder()
       {
         doAnswer(confirm()).when(presentation).notifyFolderSelectionNeeded(any(UserNotificationWithFeedback.class));
@@ -99,7 +99,7 @@ public class DefaultSiteOpenerPresentationControlTest
     /*******************************************************************************************************************
      *
      ******************************************************************************************************************/
-    @Test
+    @Test(dependsOnMethods = "initialize_must_bind_the_presentation_and_set_the_default_path_to_user_home")
     public void must_do_nothing_when_action_performed_and_the_user_cancelled_the_selection()
       {
         doAnswer(cancel()).when(presentation).notifyFolderSelectionNeeded(any(UserNotificationWithFeedback.class));
