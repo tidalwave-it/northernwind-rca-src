d0550924fdcec53b2014fb2d258f97794e898f60
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-07-15 22:15:33 +0200

    Resumed tests, added tests.

--- modules/ContentEditor/src/test/java/it/tidalwave/northernwind/rca/ui/contenteditor/impl/ResourcePropertiesBinderTest.java
+++ modules/ContentEditor/src/test/java/it/tidalwave/northernwind/rca/ui/contenteditor/impl/ResourcePropertiesBinderTest.java
@@ -32,7 +32,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import com.google.common.collect.ImmutableMap;
+import it.tidalwave.util.Key;
 import it.tidalwave.util.NotFoundException;
+import it.tidalwave.role.ui.BoundProperty;
 import it.tidalwave.northernwind.core.model.ResourceProperties;
 import it.tidalwave.northernwind.core.impl.model.DefaultResourceProperties;
 import it.tidalwave.northernwind.core.model.ModelFactory;
@@ -40,11 +43,9 @@ import it.tidalwave.northernwind.core.model.spi.ModelFactorySupport;
 import it.tidalwave.northernwind.rca.embeddedserver.EmbeddedServer.Document;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import static it.tidalwave.northernwind.rca.ui.contenteditor.impl.ResourcePropertiesBinder.*;
-import static it.tidalwave.northernwind.rca.ui.contenteditor.spi.DefaultContentEditorPresentationControl.*;
 import static it.tidalwave.util.test.FileComparisonUtils.assertSameContents;
+import static it.tidalwave.northernwind.rca.ui.contenteditor.impl.ResourcePropertiesMatcher.*;
 import static org.mockito.Mockito.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -57,6 +58,11 @@ import static org.hamcrest.CoreMatchers.*;
  **********************************************************************************************************************/
 public class ResourcePropertiesBinderTest
   {
+    private static final Key<String> PROPERTY_1 = new Key<>("property1");
+    private static final Key<String> PROPERTY_2 = new Key<>("property2");
+    private static final String ORIGINAL_PROPERTY_1_VALUE = "<html>\n<head>\n</head>\n<body>\nthe body\n</body>\n</html>";
+    private static final String ORIGINAL_PROPERTY_2_VALUE = "title";
+
     private ResourcePropertiesBinder fixture;
 
     private ResourceProperties properties;
@@ -81,9 +87,8 @@ public class ResourcePropertiesBinderTest
               }
           };
 
-        final String html = "<html>\n<head>\n</head>\n<body>\nthe body\n</body>\n</html>";
-
-        properties = modelFactory.createProperties().build().withProperty(PROPERTY_FULL_TEXT, html);
+        properties = modelFactory.createProperties().build().withProperty(PROPERTY_1, ORIGINAL_PROPERTY_1_VALUE)
+                                                            .withProperty(PROPERTY_2, ORIGINAL_PROPERTY_2_VALUE);
         callback = mock(UpdateCallback.class);
         fixture = new ResourcePropertiesBinder(properties);
       }
@@ -102,7 +107,7 @@ public class ResourcePropertiesBinderTest
 
         final File expectedProlog = new File("src/main/resources/" + EDITOR_PROLOG);
         final File expectedEpilog = new File("src/main/resources/" + EDITOR_EPILOG);
-        assertSameContents(expectedProlog , prolog);
+        assertSameContents(expectedProlog, prolog);
         assertSameContents(expectedEpilog, epilog);
       }
 
@@ -110,10 +115,42 @@ public class ResourcePropertiesBinderTest
      *
      ******************************************************************************************************************/
     @Test(dependsOnMethods = "must_properly_initialize")
-    public void must_create_a_proper_proxy_document()
+    public void must_properly_set_value_to_bound_property()
+      throws NotFoundException, IOException
+      {
+        final BoundProperty<String> boundProperty = new BoundProperty<>();
+
+        fixture.bind(PROPERTY_2, boundProperty, callback);
+
+        assertThat(boundProperty.get(), is(ORIGINAL_PROPERTY_2_VALUE));
+        verifyZeroInteractions(callback);
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(dependsOnMethods = "must_properly_initialize")
+    public void must_be_notified_with_updated_ResourceProperties_when_bound_property_updated()
+      throws NotFoundException, IOException
+      {
+        final BoundProperty<String> boundProperty = new BoundProperty<>();
+        fixture.bind(PROPERTY_2, boundProperty, callback);
+
+        boundProperty.set("New title");
+
+        verify(callback).notify(argThat(resourcePropertiesWith(map().put(PROPERTY_1, ORIGINAL_PROPERTY_1_VALUE)
+                                                                    .put(PROPERTY_2, "New title"))));
+        verifyNoMoreInteractions(callback);
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(dependsOnMethods = "must_properly_initialize")
+    public void must_properly_set_value_to_bound_document()
       throws IOException
       {
-        final Document document = fixture.createBoundDocument(PROPERTY_FULL_TEXT, callback);
+        final Document document = fixture.createBoundDocument(PROPERTY_1, callback);
 
         assertThat(document.getMimeType(), is("text/html"));
 
@@ -121,46 +158,24 @@ public class ResourcePropertiesBinderTest
         final File expectedFile = new File("src/test/resources/ExpectedDocumentProxy.txt");
         writeToFile(file, document.getContent());
         assertSameContents(expectedFile, file);
+        verifyZeroInteractions(callback);
       }
 
     /*******************************************************************************************************************
      *
      ******************************************************************************************************************/
     @Test(dependsOnMethods = "must_properly_initialize")
-    public void must_create_a_proxy_document_that_properly_updates_properties()
+    public void must_be_notified_with_updated_ResourceProperties_when_bound_document_updated()
       throws IOException
       {
-        final Document document = fixture.createBoundDocument(PROPERTY_FULL_TEXT, callback);
+        final Document document = fixture.createBoundDocument(PROPERTY_1, callback);
+        
         document.update("the updated body\n");
 
         final String expectedHtml = "<!doctype html>\n<html>\n<head>\n</head>\n<body>\nthe updated body\n</body>\n</html>";
-        verify(callback).notify(argThat(new BaseMatcher<ResourceProperties>()
-          {
-            @Override
-            public boolean matches (final Object item)
-              {
-                if (!(item instanceof ResourceProperties))
-                  {
-                    return false;
-                  }
-
-                final ResourceProperties p = (ResourceProperties)item;
-                try
-                  {
-                    return expectedHtml.equals(p.getProperty(PROPERTY_FULL_TEXT));
-                  }
-                catch (NotFoundException | IOException e)
-                  {
-                    return false;
-                  }
-              }
-
-            @Override
-            public void describeTo (final Description description)
-              {
-                description.appendText("ResourceProperties with PROPERTY_FULL_TEXT=" + expectedHtml);
-              }
-          }));
+        verify(callback).notify(argThat(resourcePropertiesWith(map().put(PROPERTY_1, expectedHtml)
+                                                                    .put(PROPERTY_2, ORIGINAL_PROPERTY_2_VALUE))));
+        verifyNoMoreInteractions(callback);
       }
 
     /*******************************************************************************************************************
@@ -176,5 +191,14 @@ public class ResourcePropertiesBinderTest
             pw.print(editorHeader);
             pw.flush();
           }
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    private static ImmutableMap.Builder<Key<String>, String> map()
+      {
+        return new ImmutableMap.Builder<>();
       }
   }
